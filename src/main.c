a12ff2a70e4c6be3008195ec508c22b69941686b
gonzalezagustin512-creator <gonzalezagustin512@gmail.com>
2026-04-28 15:14:12 -0300

    Carga inicial del TP2

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+/*
+ * Trabajo Práctico N°2: GPIO & Teclado
+ * * Asignatura: Técnicas Digitales II
+ * Alumno: Agustín González
+ * Institución: UTN - Facultad Regional Bahía Blanca
+ * * Descripción:
+ * Implementación de una máquina de estados para el escaneo de un teclado
+ * matricial de 4x4 y control de secuencias de un LED mediante intermitencias
+ * y bases de tiempo variables.
+ */
+
+#include "stm32f4xx.h"
+			
+
+int main(void)
+{
+
+	for(;;);
+}
